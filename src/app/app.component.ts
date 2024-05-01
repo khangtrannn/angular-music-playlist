b93b1cb276fe3d354f9709104d91eb92cbfeee5c
Khang Tran <khangtrann8198@gmail.com>
2024-05-01 21:05:49 +0700

    refactor code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/ui/header.component';
+import { HeaderComponent } from './shared/ui/header/header.component';
 
 @Component({
   selector: 'app-root',
